9abc9f169cd174c77ebce3c1227eec47f163c05c
AndreaChK <andreachaconk@gmail.com>
2025-10-30 11:15:41 -0600

    :rocket: Admin Controller y Vista

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace MesaListo.Controllers
+{
+    [Authorize(Roles = "Admin")] // Solo los Admin pueden entrar
+    public class AdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        // 🔹 Listado de todos los usuarios
+        public IActionResult Index()
+        {
+            var usuarios = _userManager.Users.ToList();
+            return View(usuarios);
+        }
+
+        // 🔹 Vista para editar roles
+        public async Task<IActionResult> EditRoles(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            var allRoles = _roleManager.Roles.ToList();
+
+            var model = new EditRolesViewModel
+            {
+                UserId = user.Id,
+                UserEmail = user.Email,
+                Roles = allRoles.Select(r => new RoleSelection
+                {
+                    RoleName = r.Name,
+                    Selected = userRoles.Contains(r.Name)
+                }).ToList()
+            };
+
+            return View(model);
+        }
+
+        // 🔹 Guardar los cambios de roles
+        [HttpPost]
+        public async Task<IActionResult> EditRoles(EditRolesViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.UserId);
+            if (user == null) return NotFound();
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            var selectedRoles = model.Roles.Where(r => r.Selected).Select(r => r.RoleName);
+
+            var addRoles = selectedRoles.Except(userRoles);
+            var removeRoles = userRoles.Except(selectedRoles);
+
+            await _userManager.AddToRolesAsync(user, addRoles);
+            await _userManager.RemoveFromRolesAsync(user, removeRoles);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+
+    // 🔹 Modelos de vista
+    public class EditRolesViewModel
+    {
+        public string UserId { get; set; }
+        public string UserEmail { get; set; }
+        public List<RoleSelection> Roles { get; set; } = new();
+    }
+
+    public class RoleSelection
+    {
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+    }
+}
